1419213e05d15a1c24417df061cad07511059d47
Devin MacGillivray <devhmac@gmail.com>
2021-04-13 15:24:53 -0600

    adds header.jsx story

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,7 @@ import DayList from 'components/DayList'
 import InterviewerListItem from "components/InterviewerListItem"
 import InterviewerList from "components/InterviewerList"
 import Appointment from "components/Appointment"
+import Header from "components/Appointment/header"
 
 storiesOf("Button", module)
   .addParameters({
@@ -141,3 +142,4 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time='12pm' />)
+  .add('header', () => <Header time={'3pm'} />)
