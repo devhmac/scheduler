90ee2327c45ba271a6e9c3f9de3dc984094fa271
Devin MacGillivray <devhmac@gmail.com>
2021-04-13 15:03:31 -0600

    sets up appointment components

---
+++ src/components/Appointment/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'components/Application/styles.scss'
+
+export default function Appointment(props) {
+  return(
+    <article className='appointment'>
+
+    </article>
+  );
+};
