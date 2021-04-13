2662e1470acb045276d5d6c39b3e3fda6bc9135a
Devin MacGillivray <devhmac@gmail.com>
2021-04-13 17:58:33 -0600

    successfully adds state to input value and interviewer state

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -3,8 +3,8 @@ import Button from 'components/Button';
 import InterviewerList from 'components/InterviewerList';
 
 export default function Form(props) {
-const [name, setName] = useState('')
-const [interviewer, setInterviewer] = useState(0)
+const [name, setName] = useState(props.name ||'')
+const [interviewer, setInterviewer] = useState(props.interviewer || null)
 
   return(
     <main className="appointment__card appointment__card--create">
@@ -15,14 +15,14 @@ const [interviewer, setInterviewer] = useState(0)
           name="name"
           type="text"
           placeholder="Enter Student Name"
-          onChange={(event)=>{}}
-          value={props.name}
+          onChange={(event)=>{setName(event.target.value)}}
+          value={name}
           /*
             This must be a controlled component
           */
         />
       </form>
-      <InterviewerList interviewers={props.interviewers} interviewer={props.interviewer} setInterviewer={props.setInterviewer} />
+      <InterviewerList interviewers={props.interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
     </section>
     <section className="appointment__card-right">
       <section className="appointment__actions">
