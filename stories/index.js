53a52430ff68b53346ac752cfdfb14c1034383a3
Devin MacGillivray <devhmac@gmail.com>
2021-04-13 15:36:16 -0600

    sets up empty story, passing onAdd callback

--- stories/index.js
+++ stories/index.js
@@ -11,7 +11,8 @@ import DayList from 'components/DayList'
 import InterviewerListItem from "components/InterviewerListItem"
 import InterviewerList from "components/InterviewerList"
 import Appointment from "components/Appointment"
-import Header from "components/Appointment/header"
+import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty"
 
 storiesOf("Button", module)
   .addParameters({
@@ -143,3 +144,4 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time='12pm' />)
   .add('header', () => <Header time='3pm' />)
+  .add('Empty', () => <Empty onAdd={action('onAdd')} />)
