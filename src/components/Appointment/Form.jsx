0c4da4a705e75ff3eb84418aa07156ed28f139e7
Devin MacGillivray <devhmac@gmail.com>
2021-04-13 18:24:35 -0600

    form onsave now returns interviewer and name

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -6,28 +6,40 @@ export default function Form(props) {
 const [name, setName] = useState(props.name ||'')
 const [interviewer, setInterviewer] = useState(props.interviewer || null)
 
+const reset = () =>{
+  setName('');
+  setInterviewer(null);
+}
+const cancel = () =>{
+  reset();
+  props.onCancel();
+  
+}
+
   return(
     <main className="appointment__card appointment__card--create">
     <section className="appointment__card-left">
-      <form autoComplete="off">
+      <form 
+      autoComplete="off"
+      onSubmit={event => event.preventDefault()}
+      >
         <input
           className="appointment__create-input text--semi-bold"
           name="name"
           type="text"
           placeholder="Enter Student Name"
+
+          //control input
           onChange={(event)=>{setName(event.target.value)}}
           value={name}
-          /*
-            This must be a controlled component
-          */
         />
       </form>
       <InterviewerList interviewers={props.interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
     </section>
     <section className="appointment__card-right">
       <section className="appointment__actions">
-        <Button onClick={props.onCancel} danger>Cancel</Button> 
-        <Button onClick={props.onSave} confirm>Save</Button>
+        <Button onClick={cancel} danger>Cancel</Button> 
+        <Button onClick={()=> props.onSave(name,interviewer)} confirm>Save</Button>
       </section>
     </section>
   </main>
