e0dbbafb452507814a11fa4b760ae9effaeb0570
Devin MacGillivray <devhmac@gmail.com>
2021-04-14 16:02:06 -0600

    initial logic for helper

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,1 +1,14 @@
-const getAppointmentsForDay = (state, day) => { };
+
+export function getAppointmentsForDay(state, day) {
+
+  const dayMatchArray = []
+
+  for (let day of state) {
+    if (day.name === day) {
+      console.log(day.appointment)
+    }
+  }
+
+  return dayMatchArray;
+};
+
