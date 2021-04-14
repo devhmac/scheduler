c640a5b8299ca05c7ba2d388e5d5f958622d5f69
Devin MacGillivray <devhmac@gmail.com>
2021-04-14 15:44:48 -0600

    adds helpers dir, starting selector helper function

--- src/components/Application.js
+++ src/components/Application.js
@@ -60,6 +60,9 @@ export default function Application(props) {
   const [day, setDay] = useState('Monday');
   const [days, setDays] = useState([])
 
+
+
+
   useEffect(() => {
     axios.get('/api/days')
       .then(response => setDays([...response.data]))
---
+++ src/helpers/selectors.js
@@ -0,0 +1,1 @@
+const getAppointmentsForDay = (state, day) => { };
