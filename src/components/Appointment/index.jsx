bc7b01f2713cf306fefc4604016ea0bd17c58066
Devin MacGillivray <devhmac@gmail.com>
2021-04-13 19:30:11 -0600

    successful story test if appointment in booked state

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -5,12 +5,14 @@ import Empty from './Empty'
 import Show from './Show'
 
 export default function Appointment(props) {
+  console.log(props)
   return(
     <article className='appointment'>
       <Header 
       time={props.time}
        />
-      {props.interview ?<Show /> : <Empty />}
+       {props.interview ?<Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />} 
+
     </article>
   );
 };
