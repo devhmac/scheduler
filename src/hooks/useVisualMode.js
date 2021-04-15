d1c4b59344d58bfc93ff7fa60d611a7dfdb06efd
Devin MacGillivray <devhmac@gmail.com>
2021-04-15 13:56:16 -0600

    completes the transition function and test in custom hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,5 +3,12 @@ import React, { useState } from 'react'
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial)
 
-  return { mode };
+  const transition = (newMode) => {
+    setMode(newMode)
+  }
+  const back = () => {
+
+  }
+
+  return { mode, transition };
 }
