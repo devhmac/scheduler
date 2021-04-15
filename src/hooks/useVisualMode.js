c6f354a9023c74c80128e5cd10648e6a9ea8c29e
Devin MacGillivray <devhmac@gmail.com>
2021-04-15 14:21:19 -0600

    back function and test completed

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react'
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial)
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   const transition = (newMode) => {
     setMode(newMode)
+    setHistory(prev => [...prev, newMode])
+    console.log('This is history', history)
   }
   const back = () => {
+    const prevHistory = history.splice(-1)
+    setMode(history[history.length - 1])
 
   }
 
-  return { mode, transition };
+  return { mode, transition, back };
 }
