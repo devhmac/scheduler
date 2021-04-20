098e10b87ea26d6228fc584655bcb4d2aa444dfb
Devin MacGillivray <devhmac@gmail.com>
2021-04-20 17:11:09 -0600

    test completed to check edit functionality, and ensure spots remaining dont change on save

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText, findByAltText, queryByAltText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText, findByAltText, queryByAltText, getByDisplayValue } from "@testing-library/react";
 
 import Application from "components/Application";
 import axios from '__mocks__/axios'
@@ -71,6 +71,34 @@ describe("Form", () => {
 
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   })
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
 
+    //1 - render app
+    const { container, debug } = render(<Application />);
+    //2 - wait until the text "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+
+    const appointment = getAllByTestId(container, "appointment").find(appointment => queryByText(appointment, "Archie Cohen"));
+    //3 - click edit button
+    fireEvent.click(queryByAltText(appointment, "Edit"))
+    //4 - check if archie cohen is on the screen
+    expect(getByDisplayValue(appointment, 'Archie Cohen')).toBeInTheDocument();
+    //5 - edit name, change to something else
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    //6 - click interviewer
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    //7 - click save
+    fireEvent.click(getByText(appointment, 'Save'));
+    //8 - check saving state
+    expect(getByText(appointment, 'Saving')).toBeInTheDocument();
+    //9 - wait for new name to appear
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+    //10 - check that monday has text 1 spot remaining  
+    const day = getAllByTestId(container, 'day').find(day => getByText(day, "Monday"));
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+  })
 });
 
