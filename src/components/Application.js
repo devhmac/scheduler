f8deca57a1ce43a4257778363969db94a3b09e1c
Devin MacGillivray <devhmac@gmail.com>
2021-04-21 17:11:44 -0600

    cleaning up unused vars

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import "components/Application.scss";
 import DayList from 'components/DayList'
 import Appointment from 'components/Appointment'
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from 'helpers/selectors'
-import axios from 'axios'
+
 
 import useApplicationData from 'hooks/useApplicationData'
 export default function Application(props) {
