8176e252403feb4cb73d426a510f660175e5b034
Devin MacGillivray <devhmac@gmail.com>
2021-04-21 16:17:27 -0600

    updating import style to be more verbose

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,13 +1,13 @@
 import React, {useEffect} from 'react'
 import 'components/Appointment/styles.scss'
-import Header from './Header'
-import Empty from './Empty'
-import Show from './Show'
-import Form from './Form'
+import Header from 'components/Appointment/Header'
+import Empty from 'components/Appointment/Empty'
+import Show from 'components/Appointment/Show'
+import Form from 'components/Appointment/Form'
 import useVisualMode from '../../hooks/useVisualMode'
-import Status from './Status'
-import Confirm from './Confirm'
-import Error from './Error'
+import Status from 'components/Appointment/Status'
+import Confirm from 'components/Appointment/Confirm'
+import Error from 'components/Appointment/Error'
 
 
 const EMPTY = "EMPTY";
